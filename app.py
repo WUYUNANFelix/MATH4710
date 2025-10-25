818f80828a1bad2ffbc4e12b6ad763804a00cb5e
WUYUNANFelix <yunanw@kean.edu>
2025-10-25 20:09:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from dash import Dash, dcc, html
 
 data = (
     pd.read_csv("avocado.csv")
-    .query("type == 'conventional' and region == 'Albany'")
+    .query("‘type’ == 'conventional' and region == 'Albany'")
     .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
     .sort_values(by="Date")
 )
